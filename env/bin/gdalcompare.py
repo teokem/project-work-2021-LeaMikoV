d44e530c74afa46ab963d500ea75521d12e5278c
Lea Miko Versbach <lea_miko.versbach@math.lu.se>
2021-04-10 10:07:59 +0200

    udated environment file with versions

--- env/bin/gdalcompare.py
+++
@@ -1,4 +0,0 @@
-#!/Users/leaversbach/Documents/git/project-work-2021-LeaMikoV/env/bin/python
-# EASY-INSTALL-SCRIPT: 'GDAL==3.2.2','gdalcompare.py'
-__requires__ = 'GDAL==3.2.2'
-__import__('pkg_resources').run_script('GDAL==3.2.2', 'gdalcompare.py')
--- env/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/Users/leaversbach/Documents/git/project-work-2021-LeaMikoV/env/lib"
-XML2_LIBS="-lxml2 -L/Users/leaversbach/Documents/git/project-work-2021-LeaMikoV/env/lib -lz -L/Users/leaversbach/Documents/git/project-work-2021-LeaMikoV/env/lib -llzma -lpthread -L/Users/leaversbach/Documents/git/project-work-2021-LeaMikoV/env/lib -liconv -L/Users/leaversbach/Documents/git/project-work-2021-LeaMikoV/env/lib -licui18n -licuuc -licudata -lm "
-XML2_INCLUDEDIR="-I/Users/leaversbach/Documents/git/project-work-2021-LeaMikoV/env/include/libxml2 -I/Users/leaversbach/Documents/git/project-work-2021-LeaMikoV/env/include"
-MODULE_VERSION="xml2-2.9.10"
-
